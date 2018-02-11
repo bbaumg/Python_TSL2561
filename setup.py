1a6b74f00e9173b49be9efbc407a7493be54a1e5
bbaumg <bbaumg@gmail.com>
2018-02-11 08:51:20 -0600

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name'Python_TSL2561',
+		version='v1.0.0',
+		description='Driver for TSL2561 Luminis sensor',
+		url='https://github.com/bbaumg/Python_TSL2561',
+		author='Barrett Baumgartner',
+		license='MIT',
+		packages=['TSL2561'],
+		zip_safe=False)
